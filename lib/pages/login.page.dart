1cd54742df0f10c8d858a53a636a489ab8418d37
Lucas Azevedo <lazevedo@gruposalutem.com.br>
2019-06-01 16:03:58 -0300

    Tela inicial e a pagina de login

---
+++ lib/pages/login.page.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: 400,
+      decoration: new BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [            
+            Colors.amberAccent[100],
+            Colors.amber[800],
+            ],
+          ),
+        ),
+        padding: EdgeInsets.only(
+          top: 60,
+          left: 40,
+          right: 40,
+        ),
+        child: ListView(
+          children: <Widget>[
+
+
+            Container(
+              height: 60,
+              alignment: Alignment.centerLeft,
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    child: SizedBox(
+                      child: Image.asset("assets/iw-icon.png")
+                    ),
+                  ),
+                  Text(
+                  "iWenT",
+                  style: TextStyle(
+                    fontFamily: 'Righteous',
+                    fontSize: 70,
+                    color: Colors.white,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                ],
+              ),
+            ),
+            
+            /*Container(
+                child: Text(
+                  "iWenT",
+                  style: TextStyle(
+                    fontFamily: 'Righteous',
+                    fontSize: 70,
+                    color: Colors.white,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+            ),*/
+
+            SizedBox(
+              height: 40,
+            ),
+
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,     
+              style: new TextStyle(color: Colors.white, fontSize: 20, fontFamily: 'Overpass'),       
+              decoration: InputDecoration(          
+                labelText: "E-mail",
+                labelStyle: TextStyle(
+                  fontFamily: 'Overpass',
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+
+            SizedBox(
+              height: 20,
+            ),
+
+            TextFormField(
+              keyboardType: TextInputType.text,
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: "Senha",
+                labelStyle: TextStyle(
+                  fontFamily: 'Overpass',
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+
+            SizedBox(
+              height: 40,
+            ),
+
+            Container(
+              height: 40,
+              child: FlatButton(
+                child: Text(
+                  "Cadastre-se",
+                  style: TextStyle(
+                    fontFamily: 'Overpass',
+                    color: Colors.white,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+              ),
+            ),
+
+            SizedBox(
+              height: 5,
+            ),
+
+            Container(
+               height: 60,
+               alignment: Alignment.center,
+               decoration: BoxDecoration(
+                border: new Border.all(
+                  color: Colors.white
+                ),
+                color: Colors.transparent,      
+                borderRadius: BorderRadius.all(          
+                Radius.circular(70),
+                 ), 
+               ),
+               child: SizedBox.expand(
+                 child: FlatButton(
+                   child: Row(
+                     children: <Widget>[     
+                        SizedBox(
+                          width: 90,
+                        ),                 
+                       Text(
+                         "ENTRAR",
+                         textAlign: TextAlign.left,
+                         style: TextStyle(
+                           fontFamily: 'Overpass',
+                           color: Colors.white,
+                           fontSize: 20,
+                         ),
+                       ),
+                     ],
+                   ),
+                   onPressed: () =>{},
+                 ),            
+               ),
+            ),
+
+             SizedBox(
+              height: 100,
+            ) 
+            
+
+          ],
+        ),
+               
+      ),     
+    );
+  }
+}
